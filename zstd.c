7682e751be572d819031f8635cdccfbeb2bcae21
林纯杰 <linchunjie@xmfunny.com>
2023-03-25 14:04:55 +0800

    zstd 逻辑调整

--- zstd.c
+++ zstd.c
@@ -8,17 +8,20 @@
 #define ZSTD_EXPORT extern 
 #endif
 
-ZSTD_EXPORT int32_t decompress(uint8_t *dst, int32_t dstSize, const uint8_t *src, int32_t srcSize)
+ZSTD_EXPORT size_t decompress(uint8_t *dst, int32_t dstSize, const uint8_t *src, int32_t srcSize)
 {
-  size_t result = ZSTD_decompress(dst, dstSize, src, srcSize);
-  return ZSTD_isError(result);
+  size_t decSize = ZSTD_decompress(dst, dstSize, src, srcSize);
+  if(!ZSTD_isError(decSize)){
+    return decSize;
+  }
+  return 0;
 }
 
-ZSTD_EXPORT int32_t compress(uint8_t *dst, int32_t dstSize, const uint8_t *src, int32_t srcSize, int32_t compressLevel)
+ZSTD_EXPORT size_t compress(uint8_t *dst, int32_t dstSize, const uint8_t *src, int32_t srcSize, int32_t compressLevel)
 {
-  size_t result = ZSTD_compress(dst, dstSize, src, srcSize, compressLevel);
-  if(!ZSTD_isError(result)){
-    return result;
+  size_t cSize = ZSTD_compress(dst, dstSize, src, srcSize, compressLevel);
+  if(!ZSTD_isError(cSize)){
+    return cSize;
   }
-  return -1;
+  return 0;
 }
